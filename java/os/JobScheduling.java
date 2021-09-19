3354f4dfbcdb28f165633d6c5e7239b11ae9018f
Huangdonglian233 <2957319235@qq.com>
2021-09-19 23:23:13 +0800

    suibian

--- java/os/JobScheduling.java
+++ java/os/JobScheduling.java
@@ -1,14 +1,10 @@
 package com.huang.os;
-
-import java.sql.Time;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class JobScheduling {
-
-    private int timeCount=0;
-    private int nowTime=1000;
+    private int timeCount=1000;
     private Task[] tasks=new Task[4];
     private List arr=new ArrayList();
 
@@ -35,29 +31,35 @@ public class JobScheduling {
     }
     public void run(){
         while (!isDone()){
-            canRun();
-            int key=shouldRun();
-            if(key==-1){
+
+                canRun();
+                if(judge()) {
+                    int key = shouldRun();
+                    if (key != -1) {
+                        if (!arr.contains(key))
+                            arr.add(key);
+                    }
+                }
+                shortJob(arr);
+                Print();
                 timeCount++;
-            }else {
-                excute(key);
-            }
+                System.out.println(timeCount);
         }
     }
 
     public void Print(){
         System.out.println("任务"+"   "+"\t"+"提交时间"+'\t'+"预计运行时间"+"\t"+"状态"+"\t\t"+
-                "目前运行时间"+'\t'+"完成时间"+'\t'+"周转时间"+'\t'+"带权周转时间");
+                "开始时间"+"\t\t"+"目前运行时间"+'\t'+"完成时间"+'\t'+"周转时间"+'\t'+"带权周转时间");
         for (Task task : tasks) {
             System.out.println(task.getName()+'\t'+task.getTaskIn()+"\t\t"+task.getNeedTime()
-                    +"\t\t"+task.getStatus()+"\t\t"+task.getRunTime()+"\t\t\t"+task.getFinishTime()+"\t\t"
+                    +"\t\t"+task.getStatus()+"\t\t"+task.getStartTime()+"\t\t"+task.getRunTime()+"\t\t\t"+task.getFinishTime()+"\t\t"
                     +task.gettTime()+"\t\t"+task.getwTime());
         }
     }
 
     public boolean isDone(){
         for (Task task:tasks){
-            if(task.getStatus()!=1){
+            if(task.getStatus()!=2){
                 return false;
             }
         }
@@ -66,12 +68,19 @@ public class JobScheduling {
 
     public void canRun(){
         for (Task task:tasks) {
-            if(task.getQueueTime()<timeCount&&task.getStatus()!=1){
+            if(task.getTaskIn()<=timeCount&&task.getStatus()==-1){
                 task.setStatus(0);
             }
         }
     }
-
+    public boolean judge(){
+        for (Task task:tasks) {
+            if(task.getStatus()==-1){
+                return true;
+            }
+        }
+        return false;
+    }
     public int shouldRun(){
         List<Integer> list=new ArrayList<Integer>();
         for (int i=0;i< tasks.length;i++) {
@@ -79,18 +88,24 @@ public class JobScheduling {
                 list.add(i);
             }
         }
-        int key=confirm(list);
+        int key=-1;
+        if(arr.size()<2) {
+            key = confirm(list);
+        }
         return key;
     }
 
-    public int confirm(List<Integer> list){
+    public int confirm(List<Integer> list){//计算最高响应比
         if(list.isEmpty()){
             return -1;
+        }
+        if(list.size()==1){
+            return list.get(0);
         }
         double MaxTemp = -1;
         int key = 0;
         for (int i = 0; i < list.size(); i++) {
-            double temp = (timeCount - tasks[list.get(i)].getQueueTime()) / tasks[list.get(i)].getRunTime();
+            double temp = 1 + (timeCount - tasks[list.get(i)].getTaskIn()) / tasks[list.get(i)].getQueueTime();
             if (temp > MaxTemp) {
                 MaxTemp = temp;
                 key = i;
@@ -102,12 +117,68 @@ public class JobScheduling {
     public void excute(int key) {
         tasks[key].setStartTime(timeCount);
         tasks[key].setFinishTime(timeCount + tasks[key].getRunTime());
-        tasks[key].setAvgTime((tasks[key].getFinishTime() - tasks[key].getQueueTime())
+        tasks[key].setAvgTime((tasks[key].getFinishTime() - tasks[key].getTaskIn())
                                 /tasks[key].getRunTime());
                 ;
         tasks[key].setStatus(1);
         timeCount = tasks[key].getFinishTime();
         arr.add(key);
+    }
+    public void setFinishTime(Task task){
+            task.setStatus(2);
+            task.setFinishTime(timeCount);
+            task.settTime(task.getFinishTime()- task.getTaskIn());
+            task.setwTime(task.gettTime()/task.getNeedTime());
+    }
+    public void shortJob(List<Integer> list){
+        if (list.size()<1){
+            list.add(shouldRun());
+        }
+        if (list.size()==1){
+            int temp=list.get(0);
+            if(tasks[temp].getStartTime()==0)
+                tasks[temp].setStartTime(timeCount);
+            tasks[temp].setStatus(1);
+            tasks[temp].setRunTime(timeCount-tasks[temp].getStartTime());
+            tasks[temp].setQueueTime(tasks[temp].getNeedTime()-tasks[temp].getRunTime());
+            if(tasks[temp].getQueueTime() == 0) {
+                setFinishTime(tasks[temp]);
+                System.out.println(temp);
+                arr.remove(0);
+            }
+        }
+        if (list.size()==2){
+            int temp1=list.get(0);
+            int temp2=list.get(1);
+            if(tasks[temp1].getQueueTime()<=tasks[temp2].getQueueTime()&&(tasks[temp1].getStatus()==1||tasks[2].getStatus()==1)){
+                if(tasks[temp1].getStartTime()==0)
+                    tasks[temp1].setStartTime(timeCount);
+                tasks[temp1].setStatus(1);
+//                if(tasks[temp2].getStatus()==1){
+//                    tasks[temp2].setRunTime(timeCount-tasks[temp2].getStartTime());
+//                    tasks[temp2].setQueueTime(tasks[temp2].getNeedTime()-tasks[temp2].getRunTime());
+//                }
+                tasks[temp2].setStatus(0);
+                tasks[temp1].setRunTime(timeCount-tasks[temp1].getStartTime());
+                System.out.println("RunTime"+tasks[temp1].getRunTime());
+                tasks[temp1].setQueueTime(tasks[temp1].getNeedTime()-tasks[temp1].getRunTime());
+                if(tasks[temp1].getQueueTime()==0) {
+                    setFinishTime(tasks[temp1]);
+                    arr.remove(temp1);
+                }
+            }else{
+                if(tasks[temp2].getStartTime()==0)
+                    tasks[temp2].setStartTime(timeCount);
+                tasks[temp2].setStatus(1);
+                tasks[temp1].setStatus(0);
+                tasks[temp2].setRunTime(timeCount-tasks[temp2].getStartTime());
+                tasks[temp2].setQueueTime(tasks[temp2].getNeedTime()-tasks[temp2].getRunTime());
+                if(tasks[temp2].getQueueTime()==0) {
+                    setFinishTime(tasks[temp2]);
+                    arr.remove(temp2);
+                }
+            }
+        }
     }
 }
 //JOB1 1000 30
--- java/os/Task.java
+++ java/os/Task.java
@@ -3,21 +3,17 @@ package com.huang.os;
 public class Task {
     private String name;
     private int queueTime;//任务进入作业井时间
-    private int taskIn;//任务进入时间
+    private int taskIn;//任务提交时间
     private int startTime;//任务开始时间
     private int runTime;//任务目前运行时间
     private int finishTime;//任务完成时间
     private int tTime;//周转时间
-
-
-    public void setAvgTime(int avgTime) {
-        this.avgTime = avgTime;
-    }
-
     private int needTime;//需要时间
     private double wTime;//带权周转时间
-    private int status;//0表示等待 1表示运行 -1表示未提交
+    private int status;//0表示等待 1表示运行 -1表示未提交 2表示已完成
     private int avgTime;
+    private int pauseTime;
+
     public Task(String name, int taskIn, int needTime) {
         this.name = name;
         this.taskIn = taskIn;
@@ -25,9 +21,38 @@ public class Task {
         this.startTime=0;
         this.needTime = needTime;
         this.finishTime=0;
+        this.queueTime=needTime;
         this.wTime = 0;
         this.runTime=0;
         this.status = -1;
+        this.pauseTime=0;
+    }
+
+    public int getPauseTime() {
+        return pauseTime;
+    }
+
+    public void setPauseTime(int pauseTime) {
+        this.pauseTime = pauseTime;
+    }
+
+    public void setQueueTime(int queueTime) {
+        this.queueTime = queueTime;
+    }
+    public void setAvgTime(int avgTime) {
+        this.avgTime = avgTime;
+    }
+
+    public void settTime(int tTime) {
+        this.tTime = tTime;
+    }
+
+    public void setwTime(double wTime) {
+        this.wTime = wTime;
+    }
+
+    public void setNeedTime(int needTime) {
+        this.needTime = needTime;
     }
 
     public int getQueueTime() {
@@ -49,7 +74,7 @@ public class Task {
     }
 
     public int getRunTime() {
-        return runTime-this.startTime;
+        return runTime;
     }
 
     public int getFinishTime() {
@@ -66,9 +91,17 @@ public class Task {
     }
 
     public double getwTime() {
-        if(this.status==1)
+        if(this.status==2)
         return this.tTime/this.needTime;
         else return 0;
+    }
+
+    public int getStartTime() {
+        return startTime;
+    }
+
+    public void setRunTime(int runTime) {
+        this.runTime = runTime;
     }
 
     public int getStatus() {
