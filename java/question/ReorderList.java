113bdba9ec3a044cb701715103686439607ba3cb
Huangdonglian233 <2957319235@qq.com>
2021-09-22 00:01:36 +0800

    suibian

--- java/question/ReorderList.java
+++ java/question/ReorderList.java
@@ -1,6 +1,7 @@
 package com.huang.question;
 
-import static com.huang.question.ReverseList1.reverseList;
+import java.util.ArrayList;
+import java.util.List;
 
 //给定一个单链表 L 的头节点 head ，单链表 L 表示为：
 //
@@ -24,63 +25,80 @@ public class ReorderList {
         }
     }
     public static void reorderList(ListNode head) {
-        if(head==null||head.next==null){
-            return;
-        }
-        ListNode temp=head;
-        ListNode  pre=reverse(head);
-        System.out.println();
-        if(pre==null||pre.next==null){
-            return;
-        }
         ListNode cur=head;
-        ListNode successor=head.next;
-        ListNode cur1=pre.next;
-        int sum=0;
-
-        while (successor!=null){
-            cur.next=pre;
-            pre.next=successor;
-            pre=cur1;
-            cur=successor;
-            successor=successor.next;
-            cur1=cur1.next;
-            sum++;
+        List<ListNode> list=new ArrayList<ListNode>();
+        while (cur!=null){
+            list.add(cur);
+            cur=cur.next;
         }
-        pre=head;
-        for(int i=0;i<sum;i++){
-            pre=pre.next;
+        int i=0;
+        int j=list.size()-1;
+        while (i<j){
+            list.get(i).next= list.get(j);
+            if(i==j) {
+                break;
+            }
+            i++;
+            list.get(j).next=list.get(i);
+            j--;
         }
-        pre.next=null;
-        System.out.println(sum);
-        return;
+        list.get(i).next=null;
     }
-
-    public static ListNode reverse(ListNode head){
-        ListNode temp=head;
-        ListNode head1=null;
-        head1=new ListNode(temp.val);
-        ListNode tt=head1;
-        while(temp.next!=null){
-            temp=temp.next;
-            ListNode temp1=new ListNode(temp.val);
-            tt.next=temp1;
-            tt=tt.next;
-        }
-        ListNode cur=head1.next;
-        ListNode succ = cur;
-        if(cur!=null) {
-            succ = cur.next;
-        }
-        head1.next=null;
-        while(cur!=null){
-            cur.next=head1;
-            head1=cur;
-            cur=succ;
-            if(succ!=null)
-                succ=succ.next;
-        }
-        return head1;
-    }
+//        if(head==null||head.next==null){
+//            return;
+//        }
+//        ListNode temp=head;
+//        ListNode  pre=reverse(head);
+//        if(pre==null||pre.next==null){
+//            return;
+//        }
+//        ListNode cur=head;
+//        ListNode successor=head.next;
+//        ListNode cur1=pre.next;
+//        int sum=0;
+//
+//        while (successor!=null){
+//            cur.next=pre;
+//            pre.next=successor;
+//            pre=cur1;
+//            cur=successor;
+//            successor=successor.next;
+//            cur1=cur1.next;
+//            sum++;
+//        }
+//        pre=head;
+//        for(int i=0;i<sum;i++){
+//            pre=pre.next;
+//        }
+//        pre.next=null;
+//        return;
+//    }
+//
+//    public static ListNode reverse(ListNode head){
+//        ListNode temp=head;
+//        ListNode head1=null;
+//        head1=new ListNode(temp.val);
+//        ListNode tt=head1;
+//        while(temp.next!=null){
+//            temp=temp.next;
+//            ListNode temp1=new ListNode(temp.val);
+//            tt.next=temp1;
+//            tt=tt.next;
+//        }
+//        ListNode cur=head1.next;
+//        ListNode succ = cur;
+//        if(cur!=null) {
+//            succ = cur.next;
+//        }
+//        head1.next=null;
+//        while(cur!=null){
+//            cur.next=head1;
+//            head1=cur;
+//            cur=succ;
+//            if(succ!=null)
+//                succ=succ.next;
+//        }
+//        return head1;
+//    }
 }
 
